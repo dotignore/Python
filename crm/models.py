00d1de9f42baa84f9ae47d9fd6828253375b1791
IgNOrE <hc158b@gmail.com>
2023-07-29 23:28:11 +0300

    lesson_11
    index.html add

--- crm/models.py
+++ crm/models.py
@@ -3,7 +3,12 @@ from django.db import models
 class Order(models.Model):
     order_dt = models.DateTimeField(auto_now=True)
     order_name = models.CharField(max_length=200)
-    order_phobe = models.CharField(max_length=200)
-
+    order_phone = models.CharField(max_length=200)
 
 # Create your models here.
+    def __str__(self):
+        return self.order_name
+
+    class Meta:
+        verbose_name = 'Order_'
+        verbose_name_plural = '_Orders'
--- server/urls.py
+++ server/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-#from .views import first_page
+# delete from .del_views import first_page
+from crm import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', first_page)
+    # delete     path('', first_page),
+    path('', views.first_page)
 ]
